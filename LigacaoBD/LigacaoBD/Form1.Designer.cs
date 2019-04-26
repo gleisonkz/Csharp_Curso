bb6e7d09965ab3f5ec76dda6f25776325e1ff984
gleisonkz <gleisonsubzerokz@gmail.com>
2019-04-25 23:40:38 -0300

    Aula 081 c# sql introducao a insercao de dados via c#

---
+++ LigacaoBD/LigacaoBD/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace LigacaoBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lista_dados = new System.Windows.Forms.ListBox();
+            this.text_bd = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(629, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lista_dados
+            // 
+            this.lista_dados.FormattingEnabled = true;
+            this.lista_dados.Location = new System.Drawing.Point(12, 66);
+            this.lista_dados.Name = "lista_dados";
+            this.lista_dados.Size = new System.Drawing.Size(611, 355);
+            this.lista_dados.TabIndex = 3;
+            // 
+            // text_bd
+            // 
+            this.text_bd.Location = new System.Drawing.Point(12, 30);
+            this.text_bd.Name = "text_bd";
+            this.text_bd.Size = new System.Drawing.Size(102, 20);
+            this.text_bd.TabIndex = 4;
+            this.text_bd.Text = "dados";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(629, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 55);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(629, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 55);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(629, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 55);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.text_bd);
+            this.Controls.Add(this.lista_dados);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lista_dados;
+        private System.Windows.Forms.TextBox text_bd;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ LigacaoBD/LigacaoBD/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace LigacaoBD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Faz a seleção de todos os dados contidos no banco de dados
+
+            try
+            {
+                //Conectar a Base de Dados
+                SqlCeConnection conexao = new SqlCeConnection();
+                conexao.ConnectionString = $@"Data Source = C:\Csharp\{text_bd.Text}.sdf";
+                conexao.Open();
+
+                //Selecação dos dados do BD para a Mémoria
+                var dados = new DataTable();
+                var adaptador = new SqlCeDataAdapter("SELECT * FROM Pessoas", conexao);
+                adaptador.Fill(dados);
+
+     
+
+                //Desconecta da Base de Dados
+
+                conexao.Close();
+
+                //Apresenta os dados na lista
+                lista_dados.Items.Clear();
+                foreach (DataRow linha in dados.Rows)
+                {
+                    lista_dados.Font = new Font("Courier New", 8.25F, FontStyle.Regular);
+
+                    lista_dados.Items.Add(linha["Nome"].ToString().PadRight(30) +
+                                          linha["Morada"].ToString().PadRight(30) +
+                                          linha["Telefone"].ToString()
+                          );
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Inserção de um novo registro no banco de dados
+
+
+            try
+            {
+                //Conectar a Base de Dados
+                SqlCeConnection conexao = new SqlCeConnection();
+                conexao.ConnectionString = $@"Data Source = C:\Csharp\{text_bd.Text}.sdf";
+                conexao.Open();
+
+                string query = "INSERT INTO Pessoas VALUES('Fabio Alexandre','Rua das oportunidades','987875316')";
+
+                var adaptador = new SqlCeCommand(query, conexao);
+                adaptador.ExecuteNonQuery();
+
+                //Desconecta da Base de Dados
+
+                conexao.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+
+
+        }
+    }
+}
